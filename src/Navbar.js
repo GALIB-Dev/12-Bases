8780ca9766b85417dc7d565f198efc4306d2e1a4
Mohammad Al Galib <94010323+GALIB-Dev@users.noreply.github.com>
2024-10-15 15:39:43 +0600

    Update Navbar.js

--- src/Navbar.js
+++ src/Navbar.js
@@ -35,7 +35,6 @@ const Navbar = () => {
         <li><Link to="/">Home</Link></li>
         <li><Link to="/updates">Updates</Link></li>
         <li><Link to="/contact">Contact</Link></li>
-        <li><Link to="/Resourc">Resources</Link></li>
         <li><Link to="/services">Services</Link></li>
       </ul>
     </nav>
