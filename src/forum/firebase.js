b06fc90c2bb16ff84b27b0cd24f86736443cbf83
Galib <mohammadalgalib71@gmail.com>
2024-12-29 21:47:44 +0600

    Update 20.8

--- src/forum/firebase.js
+++ src/forum/firebase.js
@@ -5,13 +5,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBtM06cTQF2EEdPqvOyyeGzyspIhKCLUMo",
-  authDomain: "bases-1125d.firebaseapp.com",
-  projectId: "bases-1125d",
-  storageBucket: "bases-1125d.firebasestorage.app",
-  messagingSenderId: "426471575892",
-  appId: "1:426471575892:web:5aa2a9d6652f08176c38c1",
-  measurementId: "G-SQ7HK3Z1QZ"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
